d71eb43d7d0f9653365fe03af730975a348cbdf2
Lucia F <l-feddersen@gmx.de>
2026-02-03 02:20:28 +0100

    working overlay

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MaintenanceComponent } from './maintenance-component/maintenance-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MaintenanceComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('goller-architekten');
+
+
 }
